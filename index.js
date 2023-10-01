b1deb94deb9d25480d22fdf5034dd610c52dc220
Jerry Tarus <jerrytarus@gmail.com>
2023-10-01 11:12:24 +0300

    First class functions done

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function functionReturned() {};
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function () {};
+  }
+  
